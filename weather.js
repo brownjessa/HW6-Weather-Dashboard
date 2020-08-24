4ec99e78452aff344465327ef7a383ff26ea7d92
Jessamine Brown <brownjessa11@gmail.com>
2020-08-24 17:56:09 -0500

    searchWeather function & api key

--- weather.js
+++ weather.js
@@ -11,3 +11,16 @@ $(document).ready(function () {
         var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
         $(".history").append(li);
       }
+      function searchWeather(searchValue) {
+        $.ajax({
+          type: "GET",
+          url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=01355dfb66b836e497ff5fde3d07af2e&units=imperial",
+          dataType: "json",
+          success: function(data) {
+            // create history link for this search
+            if (history.indexOf(searchValue) === -1) {
+              history.push(searchValue);
+              window.localStorage.setItem("history", JSON.stringify(history));
+        
+              makeRow(searchValue);
+            }
